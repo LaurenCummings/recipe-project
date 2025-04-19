411194171656b6e9dcee5a4b1344461a3a436dfe
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-19 02:06:31 -0400

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import Details from './pages/Details';
 
 function App() {
 
   return (
     <div>
-      
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
     </div>
   )
 }
