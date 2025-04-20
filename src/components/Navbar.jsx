96b5232f0be81d0abb7aba6784e71422aa37cae7
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-20 03:02:58 -0400

    Used context to access search param in navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { GlobalContext } from '../contexts/GlobalContext';
 
 function Navbar() {
+    const {searchParam, setSearchParam} = useContext(GlobalContext)
+
     return (
         <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
-            <h2 className="text-2xl font-semibold">Food Recipe</h2>
-            <NavLink to={'/'}>
-                FoodRecipe
-            </NavLink>
+            <h2 className="text-2xl font-semibold">
+                <NavLink to={'/'}>FoodRecipe</NavLink>
+            </h2>
             <form>
                 <input 
                     type="text"
                     name="search"
+                    value={searchParam}
+                    onChange={(event) => setSearchParam(event.target.value)}
                     placeholder="Enter Items..."
                     className="bg-white/75 p-3 px-8 rounded-full outline-none lg:2-96 shadow-lg shadow-red-100 focus:shadow-red-200"
                 />
