fd20de058f65cdd8ecd0dec0f56d9ac7567e25c6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-20 02:41:50 -0400

    Added links to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import { NavLink } from 'react-router-dom';
+
 function Navbar() {
     return (
         <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
             <h2 className="text-2xl font-semibold">Food Recipe</h2>
+            <NavLink to={'/'}>
+                FoodRecipe
+            </NavLink>
             <form>
                 <input 
                     type="text"
@@ -10,6 +15,14 @@ function Navbar() {
                     className="bg-white/75 p-3 px-8 rounded-full outline-none lg:2-96 shadow-lg shadow-red-100 focus:shadow-red-200"
                 />
             </form>
+            <ul className="flex gap-5">
+                <li>
+                    <NavLink to={'/'} className="text-black hover:text-gray-700 duration-300">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/favorites'} className="text-black hover:text-gray-700 duration-300">Favorites</NavLink>
+                </li>
+            </ul>
 
         </nav>
     )
