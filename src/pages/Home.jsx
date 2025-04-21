8d3c5964ae8627f8b05035b549838afc5c24b258
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-21 03:11:50 -0400

    Added recipe name and link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     return (
         <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
             {recipeList && recipeList.length > 0 
-                ? recipeList.map((item) => <RecipeItem item={item} />)
+                ? recipeList.map((item) => <RecipeItem key={item.id} item={item} />)
                 : <div>
                     <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
                         Nothing to show. Please search something
