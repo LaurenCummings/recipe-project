54cf284d5008686478a7fd2c5b1f8d66d8f99966
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-22 02:11:26 -0400

    Added api call for recipe details

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,8 +1,21 @@
+import { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { GlobalContext } from '../contexts/GlobalContext';
 
 function Details() {
-    const params = useParams();
-    console.log(params);
+    const {id} = useParams();
+    const {recipeDetailsData, setRecipeDetailsData} = useContext(GlobalContext)
+
+    useEffect(() => {
+        async function getRecipeDetails() {
+            const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+            const data = await response.json();
+
+            console.log(data);
+        }
+
+        getRecipeDetails();
+    },[])
 
     return (
         <div>
